b36e023a324ddaf433f581ea1939a2c4f9ade64d
Jesse <xwh9004@163.com>
2022-01-29 15:48:20 +0800

    角色服务类实现

--- src/main/java/com/wms/admin/service/impl/RoleServiceImpl.java
+++ src/main/java/com/wms/admin/service/impl/RoleServiceImpl.java
@@ -1,16 +1,28 @@
 package com.wms.admin.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.wms.admin.commom.PageQuery;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.wms.admin.auth.UserInfoContext;
+import com.wms.admin.commom.PageParam;
 import com.wms.admin.entity.RoleEntity;
 import com.wms.admin.mapper.RoleMapper;
 import com.wms.admin.service.IRoleService;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.wms.admin.util.UUIDUtil;
+import com.wms.admin.vo.RoleQueryVO;
 import com.wms.admin.vo.RoleVO;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import static com.wms.admin.commom.WMSConstants.DEL_FLG_1;
 
 /**
  * <p>
@@ -23,14 +35,16 @@ import java.util.List;
 @Service
 public class RoleServiceImpl extends ServiceImpl<RoleMapper, RoleEntity> implements IRoleService {
 
-
+    @Transactional
     @Override
     public boolean addRole(RoleVO roleVO) {
         checkRole(roleVO);
         RoleEntity roleEntity = new RoleEntity();
-        BeanUtils.copyProperties(roleVO,roleEntity);
-
-        return false;
+        BeanUtils.copyProperties(roleVO, roleEntity);
+        roleEntity.setId(UUIDUtil.uuid());
+        roleEntity.setCreateBy(UserInfoContext.getUsername());
+        roleEntity.setUpdateBy(UserInfoContext.getUsername());
+        return save(roleEntity);
     }
 
     private void checkRole(RoleVO roleVO) {
@@ -38,21 +52,55 @@ public class RoleServiceImpl extends ServiceImpl<RoleMapper, RoleEntity> impleme
 
     @Override
     public boolean updateRole(RoleVO roleVO) {
-        return false;
+        checkRoleForUpdate(roleVO);
+        RoleEntity roleEntity = new RoleEntity();
+        BeanUtils.copyProperties(roleVO, roleEntity);
+        roleEntity.setUpdateBy(UserInfoContext.getUsername());
+        return updateById(roleEntity);
+    }
+
+    private void checkRoleForUpdate(RoleVO roleVO) {
     }
 
     @Override
-    public boolean deleteRole(RoleVO roleVO) {
+    public boolean deleteRole(String roleId) {
         return false;
     }
 
     @Override
     public List<RoleVO> roleList() {
-        return null;
+        QueryWrapper<RoleEntity> queryWrapper = new QueryWrapper<>();
+        queryWrapper.lambda().eq(RoleEntity::getDelFlag, DEL_FLG_1);
+        List<RoleEntity> list = list(queryWrapper);
+        List<RoleVO> roleList = list.stream().map(item -> {
+            RoleVO roleVO = new RoleVO();
+            BeanUtils.copyProperties(item, roleVO);
+            return roleVO;
+        }).collect(Collectors.toList());
+        return roleList;
     }
 
     @Override
-    public IPage<RoleVO> rolePage(PageQuery pageQuery) {
-        return null;
+    public IPage<RoleVO> rolePage(RoleQueryVO roleQueryVO, PageParam pageParam) {
+        IPage<RoleEntity> page = new Page<>(pageParam.getPage(), pageParam.getLimit());
+        LambdaQueryWrapper<RoleEntity> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(RoleEntity::getDelFlag, DEL_FLG_1);
+        if (StringUtils.isNotBlank(roleQueryVO.getRoleName())) {
+            queryWrapper.like(RoleEntity::getRoleName, roleQueryVO.getRoleName());
+        }
+        if (StringUtils.isNotBlank(roleQueryVO.getRoleCode())) {
+            queryWrapper.like(RoleEntity::getRoleCode, roleQueryVO.getRoleCode());
+        }
+        if (StringUtils.isNotBlank(roleQueryVO.getType())) {
+            queryWrapper.eq(RoleEntity::getType, roleQueryVO.getType());
+        }
+        queryWrapper.orderByDesc(RoleEntity::getCreateTime);
+        page = page(page, queryWrapper);
+        IPage<RoleVO> resultPage = page.convert(entity -> {
+            RoleVO roleVO = new RoleVO();
+            BeanUtils.copyProperties(entity, roleVO);
+            return roleVO;
+        });
+        return resultPage;
     }
 }
