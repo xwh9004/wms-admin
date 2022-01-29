6faf2912bdb069a861da44367d5580310203a8b3
Jesse <xwh9004@163.com>
2022-01-29 09:32:23 +0800

    json null和empty字段 不序列化

--- src/main/java/com/wms/admin/config/WebConfig.java
+++ src/main/java/com/wms/admin/config/WebConfig.java
@@ -1,15 +1,16 @@
 package com.wms.admin.config;
 
-import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.wms.admin.interceptor.UserInfoInterceptor;
+import lombok.val;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Iterator;
 import java.util.List;
 
 @Configuration
@@ -28,5 +29,20 @@ public class WebConfig implements WebMvcConfigurer {
                         "/v2/**",
                         "/swagger-resources/**",
                         "/webjars/**");
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        Iterator<HttpMessageConverter<?>> iterator = converters.iterator();
+        while(iterator.hasNext()){
+            HttpMessageConverter<?> converter = iterator.next();
+            if(converter instanceof MappingJackson2HttpMessageConverter){
+                MappingJackson2HttpMessageConverter  mappingJackson2HttpMessageConverter=
+                        MappingJackson2HttpMessageConverter.class.cast(converter);
+
+                final ObjectMapper objectMapper = mappingJackson2HttpMessageConverter.getObjectMapper();
+                objectMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+            }
+        }
     }
 }
