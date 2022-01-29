2450be2a443a21c00bf3ff1586c4f2b404a33633
Jesse <xwh9004@163.com>
2022-01-29 15:48:59 +0800

    公共常量定义

---
+++ src/main/java/com/wms/admin/commom/WMSConstants.java
@@ -0,0 +1,13 @@
+package com.wms.admin.commom;
+
+public class WMSConstants {
+
+    public static final String DEL_FLG_1="1";
+    public static final String DEL_FLG_0="0";
+    public static final String STATUS_1 = "1";
+    public static final String STATUS_0= "0";
+    public static final String RESOURCE_TYPE_MENU="1";
+    public static final String RESOURCE_TYPE_BTN="2";
+
+
+}
