6b09e62b372265fe99ef620a09d03f1d8082382c
Jesse <xwh9004@163.com>
2022-01-29 09:33:30 +0800

    菜单测试类

--- src/test/java/com/wms/admin/service/MenuServiceTest.java
+++ src/test/java/com/wms/admin/service/MenuServiceTest.java
@@ -1,31 +1,36 @@
 package com.wms.admin.service;
 
 import com.wms.admin.entity.MenuEntity;
-import com.wms.admin.mapper.MenuMapper;
 import com.wms.admin.util.UUIDUtil;
 import com.wms.admin.vo.MenuVO;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.util.List;
 
-@SpringBootTest
+@RunWith(PowerMockRunner.class)
 public class MenuServiceTest {
 
-    @Autowired
+    @InjectMocks
     IMenuService menuService;
 
     @Test
     public void insertMenu(){
-        String name ="全景视图";
-        String code ="dashboard";
-        int level =1;
-        int seq =1;
-        String url ="/wms-admin/dashboard";
-        MenuEntity menu = createMenu(name,code,level,seq,url);
+        String name ="仓库作业";
+        String code ="operate";
+        String url ="/wms-admin/operate";
+        MenuEntity menu = topMenu(name, code, url);
         menuService.save(menu);
     }
+    
+    private MenuEntity topMenu(String name,String code,String url){
+        int level =1;
+        int seq =1;
+        return createMenu(name,code,level,seq,url);
+    }
+ 
 
     public MenuEntity createMenu(String name,String code,int level,int seq,String url){
         MenuEntity menu = new MenuEntity();
