c718ce56ea915ed0c04407aca3172b9508d3714e
Jesse <xwh9004@163.com>
2022-08-17 17:42:07 +0800

    日志配置输出到文件

--- src/main/java/com/wms/admin/entity/RoleMenuEntity.java
+++ src/main/java/com/wms/admin/entity/RoleMenuEntity.java
@@ -1,6 +1,8 @@
 package com.wms.admin.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.wms.admin.entity.BaseEntity;
 
@@ -18,7 +20,7 @@ public class RoleMenuEntity extends BaseEntity {
     /**
      * 角色ID
      */
-    @TableField("role_id")
+    @TableId(value = "role_id", type = IdType.ASSIGN_UUID )
     private String roleId;
 
     /**
