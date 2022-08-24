f901dc79873371fdbdc85db3d16715cd27294f2e
Jesse <xwh9004@163.com>
2022-08-24 15:12:16 +0800

    删除多余修饰符

--- src/main/java/com/wms/admin/commom/WMSConstants.java
+++ src/main/java/com/wms/admin/commom/WMSConstants.java
@@ -4,43 +4,42 @@ import java.math.BigDecimal;
 
 public interface WMSConstants {
 
-    public static String REQUEST_NO = "requestNo";
-
+    String REQUEST_NO = "requestNo";
 
     /**
      * 不删除
      */
-    public static final String DEL_FLG_N ="1";
+     String DEL_FLG_N ="1";
     /**
      * 已删除
      */
-    public static final String DEL_FLG_Y ="0";
-    public static final String STATUS_1 = "1";
-    public static final String STATUS_0= "0";
+     String DEL_FLG_Y ="0";
+     String STATUS_1 = "1";
+     String STATUS_0= "0";
 
-    public static final String TAKE_IN_INIT = "0";
-    public static final String TAKEN_IN= "1";
+     String TAKE_IN_INIT = "0";
+     String TAKEN_IN= "1";
 
     /**
      * 合同未生效
      */
-    public static final String CONTRACT_EDITABLE = "0";
+     String CONTRACT_EDITABLE = "0";
     /**
      * 合同已生效
      */
-    public static final String CONTRACT_EFFECT= "1";
+     String CONTRACT_EFFECT= "1";
     /**
      * 合同已失效
      */
-    public static final String CONTRACT_INEFFECTIVE= "2";
+     String CONTRACT_INEFFECTIVE= "2";
     /**
      * 合同已国期
      */
-    public static final String CONTRACT_EXPIRED= "3";
+     String CONTRACT_EXPIRED= "3";
 
-    public static final String MENU_TYPE_DIR="0";
-    public static final String MENU_TYPE_MENU ="1";
-    public static final String MENU_TYPE_RESOURCE ="2";
-    public static final BigDecimal ONE_HUNDRED = BigDecimal.valueOf(100);
+     String MENU_TYPE_DIR="0";
+     String MENU_TYPE_MENU ="1";
+     String MENU_TYPE_RESOURCE ="2";
+     BigDecimal ONE_HUNDRED = BigDecimal.valueOf(100);
 
 }
