de18e5dbbbf8f03d6e179ab1259658f70fd68584
Jesse <xwh9004@163.com>
2022-08-24 14:34:17 +0800

    收货单筛选条件

--- src/main/java/com/wms/admin/vo/TakeInQueryVO.java
+++ src/main/java/com/wms/admin/vo/TakeInQueryVO.java
@@ -1,9 +1,37 @@
 package com.wms.admin.vo;
 
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
 /**
  * @author: xwh90
  * @date: 2022/8/22 17:57
  * @description:
  */
-public class TakeInQueryVO {
+@Data
+public class TakeInQueryVO implements Serializable {
+    /**
+     * 收货单号
+     */
+    private String takeInNo;
+    /**
+     * 合同编号
+     */
+    private String contractNo;
+    /**
+     * 合同单位
+     */
+    private String contractCompany;
+    /**
+     * 收货开始时间
+     */
+    private LocalDateTime takeInStartTime;
+    /**
+     * 收货结束时间
+     */
+    private LocalDateTime takeInEndTime;
 }
