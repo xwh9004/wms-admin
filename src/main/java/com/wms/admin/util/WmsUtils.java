9555be57ad233612896d6ce5750d91a5594bd329
Jesse <xwh9004@163.com>
2022-08-24 14:33:34 +0800

    时间范围校验

--- src/main/java/com/wms/admin/util/WmsUtils.java
+++ src/main/java/com/wms/admin/util/WmsUtils.java
@@ -1,4 +1,18 @@
 package com.wms.admin.util;
 
+import com.wms.admin.commom.ResultCode;
+import com.wms.admin.exception.BusinessException;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
 public class WmsUtils {
+
+    public static void checkDateRange(LocalDateTime startTime, LocalDateTime endTime) {
+        if(Objects.nonNull(startTime) && Objects.nonNull(endTime)){
+            if(startTime.isAfter(endTime)){
+                throw new BusinessException(ResultCode.PARAM_ERROR,"开始时间不能晚于结束时间");
+            }
+        }
+    }
 }
